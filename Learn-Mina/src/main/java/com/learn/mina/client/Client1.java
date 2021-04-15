2d6c872b07e3ae8318a8af09093e5289392d4d3d
Lys <763644568@qq.com>
2021-04-15 17:32:10 +0800

    2021-04-15

--- Learn-Mina/src/main/java/com/learn/mina/client/Client1.java
+++ Learn-Mina/src/main/java/com/learn/mina/client/Client1.java
@@ -1,51 +1,23 @@
 package com.learn.mina.client;
 
-import java.io.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class Client1 {
-    public static final String IP_ADDR = "127.0.0.1";
-    public static final int PORT = 8088;
 
-    public static void main(String[] args) {
-
-        while (true) {
-            Socket socket = null;
-            try {
-                //创建一个流套接字并将其连接到指定主机上的指定端口号
-                socket = new Socket(IP_ADDR, PORT);
-
-                //读取服务器端数据
-                DataInputStream input = new DataInputStream(socket.getInputStream());
-                //向服务器端发送数据
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                System.out.print("请输入: \t");
-                String str = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                out.writeUTF(str);
-
-                String ret = input.readUTF();
-                System.out.println("服务器端返回过来的是: " + ret);
-                // 如接收到 "OK" 则断开连接
-                if ("OK".equals(ret)) {
-                    System.out.println("客户端将关闭连接");
-                    Thread.sleep(500);
-                    break;
-                }
-                out.close();
-                input.close();
-            } catch (Exception e) {
-                System.out.println("客户端异常:" + e.getMessage());
-            } finally {
-                if (socket != null) {
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        socket = null;
-                        System.out.println("客户端 finally 异常:" + e.getMessage());
-                    }
-                }
-            }
-        }
+    public static void main(String[] args) throws IOException {
+        Socket socket=new Socket("127.0.0.1",8088);
+        InputStream is=socket.getInputStream();
+        OutputStream os=socket.getOutputStream();
+        os.write("quit\n".getBytes());
+        os.flush();
+        byte[] arr=new byte[1024];
+        int len=is.read(arr);
+        System.out.println(new String(arr,0,len));
+        is.close();
+        os.close();
     }
 }
--- MQTT/netty-mqtt/mqtt-broker/src/test/java/com/learn/mqtt/netty/broker/core/MqttTest.java
+++ MQTT/netty-mqtt/mqtt-broker/src/test/java/com/learn/mqtt/netty/broker/core/MqttTest.java
@@ -1,5 +1,8 @@
 package com.learn.mqtt.netty.broker.core;
 import com.learn.mqtt.netty.broker.MqttBrokerApplication;
+
+import java.util.List;
+
 public class MqttTest {
     public static void main(String[] args) throws Exception {
         MqttBrokerApplication broker = new MqttBrokerApplication.Build()
