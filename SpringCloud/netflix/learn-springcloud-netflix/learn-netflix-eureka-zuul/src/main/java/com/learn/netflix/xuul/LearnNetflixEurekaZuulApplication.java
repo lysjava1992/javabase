749b5e86ab24fe5a615b754c6f73da4e1de7c533
Lys <763644568@qq.com>
2021-12-07 17:24:02 +0800

    zuul

---
+++ SpringCloud/netflix/learn-springcloud-netflix/learn-netflix-eureka-zuul/src/main/java/com/learn/netflix/xuul/LearnNetflixEurekaZuulApplication.java
@@ -0,0 +1,13 @@
+package com.learn.netflix.xuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class LearnNetflixEurekaZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnNetflixEurekaZuulApplication.class,args);
+    }
+}
---
+++ SpringCloud/netflix/learn-springcloud-netflix/learn-spring-cloud-stream/src/main/java/learn/spring/cloud/stream/LearnSpringCloudStreamApplication.java
@@ -0,0 +1,11 @@
+package learn.spring.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringCloudStreamApplication.class,args);
+    }
+}
